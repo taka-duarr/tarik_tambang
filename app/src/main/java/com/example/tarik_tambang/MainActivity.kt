f2ae7c5ae1f06ff6ba8e0a259f100572f9699014
Firman Ardiansyah <firman@example.com>
2025-11-10 13:34:22 +0700

    game run done

--- app/src/main/java/com/example/tarik_tambang/MainActivity.kt
+++ app/src/main/java/com/example/tarik_tambang/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.google.firebase.database.*
 
+//tes
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
